d8bd07e30625188e951763f9d6ca9ae387695f97
roaris <one0803kyopro@gmail.com>
2021-10-18 17:23:47 +0900

    リストへの追加処理を実装

--- main.js
+++ main.js
@@ -20,4 +20,27 @@ var app = new Vue({
   data: {
     todos: [],
   },
+  methods: {
+    doAdd: function() {
+      var comment = this.$refs.comment;
+      if (!comment.value.length) return;
+      this.todos.push({
+        id: todoStorage.uid++,
+        comment: comment.value,
+        state: 0
+      });
+      comment.value = '';
+    }
+  },
+  watch: {
+    todos: {
+      handler: function(todos) {
+        todoStorage.save(todos);
+      },
+      deep: true,
+    }
+  },
+  created() {
+    this.todos = todoStorage.fetch()
+  }
 });
